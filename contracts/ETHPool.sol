bc66945e6e909f53bfeec18720cf4a81d856df27
jeremyjin902 <jeremyjin902@gmail.com>
2022-03-12 20:31:58 -0500

    Structure & Install Dependencies

---
+++ contracts/ETHPool.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract ETHPool is Ownable {
+    using SafeMath for uint256;
+}
